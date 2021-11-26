d331b09cc94fff36873e144c9001d66b4cf5f443
aouwt <74026992+aouwt@users.noreply.github.com>
2021-11-25 20:54:04 -0500

    .

--- bs.c
+++ bs.c
@@ -2,16 +2,27 @@
 #include <stdlib.h>
 
 typedef unsigned char uchar_t;
-typedef uchar_t card_t;
+typedef char card_t;
 
-struct player {
-	card_t hand [53];
-	uchar_t sz;
-};
+typedef struct _deck {
+	card_t card [54];
+	uchar_t size = 0;
+} deck_t;
 
-card_t Discard[53] = 0; uchar_t Discard_size = 0;
-struct player *Player;
+typedef struct player {
+	deck_t hand;
+} player_t;
 
+deck_t Discard;
+player_t *Player;
+
+
+void newdeck (deck_t* deck) {
+}
+
+void shuffledeck (deck_t* deck) {
+	
+}
 
 void init_game (uchar_t players) {
 	Player = (struct player*) malloc (sizeof (struct player) * players);
